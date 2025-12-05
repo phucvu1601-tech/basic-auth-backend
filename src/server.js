0ca91d9cf11b5d1711a93d405d894b0f1e6936e1
Tech - Phuc Vu <huuphucvu1601@gmail.com>
2025-12-05 16:42:39 +0700

    v1.1 basic authentication
    * feat: init express server and connect to mongoDB
    
    * feat: add signup
    
    * feat: add signin
    
    * feat: add signout
    
    * feat: add getMe with middleware
    
    * feat: add env example
    
    * feat: add refresh token
    
    * feat: add swagger
    
    ---------
    
    Co-authored-by: tms-phucvu1601 <phuc.vu@tomosia.com>

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
 import cookieParser from "cookie-parser";
 import { verifyToken } from './middlewares/authMiddleware.js';
+import swaggerUI from 'swagger-ui-express';
+import YAML from "yaml";
+import fs from 'fs';
 
 dotenv.config()
 const app = express();
@@ -13,6 +16,10 @@ const PORT = process.env.PORT || 5000;
 //middleware
 app.use(express.json());
 app.use(cookieParser());
+
+//swagger
+const swaggerDocs = YAML.parse(fs.readFileSync("./src/openapi.yaml", "utf-8"));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 //public routes
 app.use('/api/auth', authRoute);
@@ -23,6 +30,6 @@ app.use("/api/user", userRoute);
 
 connectDB().then(()=>{
     app.listen(PORT, ()=>{
-        console.log(`Server is running in port ${PORT}`)
+        console.log(`- Server: http://localhost:${PORT}\n- Swagger: http://localhost:${PORT}/api-docs`)
     })    
 })
